ededca11851cb6bef25e9df31fd763b84671cfce
Lily Tran <tran.lily8@gmail.com>
2022-07-06 09:50:30 -0700

    first commit

---
+++ DreamJournal/DreamJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamJournalApp.swift
+//  DreamJournal
+//
+//  Created by Lily Tran on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DreamJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
